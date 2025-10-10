345f4133bac6688cbce1f0955fb67c19124d3e45
Mallapragada Naga Sai Srinivasa Nikhil <nikhil.m@encora.com>
2025-10-10 12:03:41 +0530

    added Employee Emp Service and Employee Order Service

---
+++ 10-10-2025/Swagger with rest template/Emp-Employee-Service/src/main/java/com/employeetest/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.employeetest.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping("/getOrder/{id}")
+	public Map<String, Object>getOrderdetails(@PathVariable int id){
+		String url = "http://localhost:8081/Order/"+id;
+		Map<String, Object> response = restTemplate.getForObject(url, Map.class);
+		response.put("message", "order fetched successfully");
+		return response;
+		
+	}
+	@GetMapping("/Info")
+	public String getOderInfo() {
+		return "Order Completed";
+	}
+	
+	
+
+}
